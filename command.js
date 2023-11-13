5378163bfe987ced5c22e6f0d217aa4d4b73cb0e
ATHEX-MD <149071051+Athe45@users.noreply.github.com>
2023-11-13 13:37:44 +0530

    Update command.js

--- command.js
+++ command.js
@@ -4,13 +4,13 @@
 ░█─── ░█─░█ ░█─░█ ░█▄▄█ ░█─░█ ─░█── ░█─░█ ── ░█──░█ ░█▄▄▀
 
 
-* @project_name PRABATH-MD
+* @project_name ATHEX-MD
 * @author Prabath Kumara <https://github.com/prabathLK>
 * @description The main hope of creating this bot is to take full advantage of the WhatsApp app and make its work easier
 * @link <https://github.com/prabathLK/PRABATH-MD>
 * @version 1.0.0
 
-© 2023 Prabath Kumara, SACHI-BOT. All rights reserved.
+© 2023 Prabath Kumara. All rights reserved.
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.*/
 
