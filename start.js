a5f08f170c5809bec29ef659752cf4345b9d8d30
Allan George <143169885+Allan13062@users.noreply.github.com>
2024-03-14 09:06:53 +0300

    Update start.js

--- start.js
+++ start.js
@@ -5,8 +5,8 @@
 ░█─── ░█─░█ ░█─░█ ░█▄▄█ ░█─░█ ─░█── ░█─░█ ── ░█──░█ ░█▄▄▀
 
 
-* @project_name PRABATH-MD
-* @authors Prabath Kumara & sachibot-team(Sachintha) <https://github.com/prabathLK>
+* @project_name ALLAN-MD 
+* @authors Allan George & sachibot-team(Sachintha) <https://github.com/prabathLK>
 * @description The main hope of creating this bot is to take full advantage of the WhatsApp app and make its work easier
 * @link <https://github.com/prabathLK/PRABATH-MD>
 * @version 2.0.6
