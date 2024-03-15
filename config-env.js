25ef6e1eb3e3c79e7cefdc81182871d027618778
PRABATH KUMARA <106251140+prabathLK@users.noreply.github.com>
2024-03-15 11:05:14 +0530

    PRABATH-MD (2.0.6) 🎉

---
+++ config-env.js
@@ -0,0 +1,28 @@
+/**
+░█▀▀█ ░█▀▀█ ─█▀▀█ ░█▀▀█ ─█▀▀█ ▀▀█▀▀ ░█─░█ ── ░█▀▄▀█ ░█▀▀▄ 
+░█▄▄█ ░█▄▄▀ ░█▄▄█ ░█▀▀▄ ░█▄▄█ ─░█── ░█▀▀█ ▀▀ ░█░█░█ ░█─░█ 
+░█─── ░█─░█ ░█─░█ ░█▄▄█ ░█─░█ ─░█── ░█─░█ ── ░█──░█ ░█▄▄▀
+
+* @project_name PRABATH-MD
+* @authors Prabath Kumara & sachibot-team(Sachintha) <https://github.com/prabathLK>
+* @description The main hope of creating this bot is to take full advantage of the WhatsApp app and make its work easier
+* @link <https://github.com/prabathLK/PRABATH-MD>
+* @version 2.0.6
+
+© 2023 Prabath Kumara, SACHI-BOT. All rights reserved.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.*/
+
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+SESSION_ID: process.env.SESSION_ID || "Enter your session-id",
+BOT_NUMBER: process.env.BOT_NUMBER || "Enter your botnumber (without + mark and spaces)",
+PASSWORD: process.env.PASSWORD || "Enter you like password",
+GITHUB_USERNAME: process.env.GITHUB_USERNAME || "Enter your github username",
+GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN || "Enter your GITHUB_AUTH_TOKEN"
+};
