9ac8fa5d253f99520e47f59723cb0233fff5a0e9
godakanda59 <139145269+godakanda59@users.noreply.github.com>
2024-03-27 01:37:34 +0530

    Update config-env.js

--- config-env.js
+++ config-env.js
@@ -3,13 +3,11 @@
 ░█▄▄█ ░█▄▄▀ ░█▄▄█ ░█▀▀▄ ░█▄▄█ ─░█── ░█▀▀█ ▀▀ ░█░█░█ ░█─░█ 
 ░█─── ░█─░█ ░█─░█ ░█▄▄█ ░█─░█ ─░█── ░█─░█ ── ░█──░█ ░█▄▄▀
 
-* @project_name PRABATH-MD
-* @authors Prabath Kumara & sachibot-team(Sachintha) <https://github.com/prabathLK>
-* @description The main hope of creating this bot is to take full advantage of the WhatsApp app and make its work easier
-* @link <https://github.com/prabathLK/PRABATH-MD>
+* @project_name R Y GODAKANDA-MD
+* @authors Ravindu Godakanda
 * @version 2.0.6
 
-© 2023 Prabath Kumara, SACHI-BOT. All rights reserved.
+© 2023 Ravindu,All rights reserved.
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.*/
 
