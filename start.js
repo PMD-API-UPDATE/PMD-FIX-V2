8bf8ea7b16ee52ddabe334a4865085fd63553f24
godakanda59 <139145269+godakanda59@users.noreply.github.com>
2024-03-27 01:48:11 +0530

    Update start.js

--- start.js
+++ start.js
@@ -5,13 +5,12 @@
 ░█─── ░█─░█ ░█─░█ ░█▄▄█ ░█─░█ ─░█── ░█─░█ ── ░█──░█ ░█▄▄▀
 
 
-* @project_name PRABATH-MD
-* @authors Prabath Kumara & sachibot-team(Sachintha) <https://github.com/prabathLK>
+* @project_name RAVINDU-MD
+* @authors Ravindu Godakanda
 * @description The main hope of creating this bot is to take full advantage of the WhatsApp app and make its work easier
-* @link <https://github.com/prabathLK/PRABATH-MD>
 * @version 2.0.6
 
-© 2023 Prabath Kumara, SACHI-BOT. All rights reserved.
+© 2023 Ravindu Godakanda. All rights reserved.
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.*/
 
