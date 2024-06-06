56a2170d116b9587b70f2aad9dbf32112d4376ed
PMD-API-UPDATE <159437074+PMD-API-UPDATE@users.noreply.github.com>
2024-06-07 00:53:00 +0530

    Update config-env.js

--- config-env.js
+++ config-env.js
@@ -1,12 +1,16 @@
+
 const fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
+
+const username = fs.readFileSync('github_username.txt', 'utf8').trim()
+
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,
-BOT_NUMBER: process.env.BOT_NUMBER,
-GITHUB_USERNAME: process.env.GITHUB_USERNAME,
-GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN,
+    SESSION_ID:  process.env.SESSION_ID,    
+    BOT_NUMBER:  process.env.BOT_NUMBER,
+    GITHUB_USERNAME: username,
+    GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN,
 };
